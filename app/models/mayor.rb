d2557fcba5c30315570dc7c3615a13ebc41c19f8
Daniel Xutuc <danielxutuc@gmail.com>
2019-04-28 23:57:07 -0300

    Relacion Municipalidad Mayor, con validacion

--- app/models/mayor.rb
+++ app/models/mayor.rb
@@ -1,2 +1,6 @@
 class Mayor < ApplicationRecord
+    has_one :municipality
+    def full_name
+        self.name + " " + self.last_name
+    end
 end
--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -1,3 +1,4 @@
 class Municipality < ApplicationRecord
   belongs_to :mayor
+  validates_uniqueness_of :mayor_id
 end
