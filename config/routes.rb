5fefa96db56d30be707ebcbbcdb5c10e49fb3d65
Daniel Xutuc <danielxutuc@gmail.com>
2019-04-29 01:00:57 -0300

    mostar las municipalidades en el home por defecto

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :neighborhoods
   resources :municipalities
   resources :mayors
+  root :to => "municipalities#index" # mostrar las municipalidades por defecto
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
